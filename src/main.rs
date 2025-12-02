f10eeb5ad8a1e4eba98f8d9db3919f36de18fe06
fdlgs <fdesloges@hotmail.com>
2025-12-02 01:22:34 -0500

    sans defmt

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 #![no_std]
 #![no_main]
 
-use cortex_m as _;
-use defmt_rtt as _;
-use defmt::*;
 // Halte notre programme en cas de panic!()
 use panic_halt as _;
 
 use rp_pico as bsp;     // Board Support Package
-
 // La macro qui débute notre programme
 use bsp::entry;
 use embedded_hal::{
@@ -25,12 +21,9 @@ mod pi_pico;
 ///
 /// Cette fonction capture les ressources du Pi Pico en vue d'écrire un programme simple
 #[entry]
-fn main() -> ! {
-
-    info!("Débutons.");
-
+fn main_loop() -> ! {
+    
     //  Capturer les ressources du Pi Pico
-
     let mut pi_pico = pi_pico::PiPico::take().unwrap();
     let pins = pi_pico.init_pins().unwrap();
     let mut timer = pi_pico.init_timer().unwrap();
